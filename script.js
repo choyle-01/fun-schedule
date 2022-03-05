ece639ff8852d6b4da81d7ed545da384590b0324
Michael Hoyle <mcalebhoyle@gmail.com>
2022-03-05 00:24:50 -0500

    finished project and filled out README

--- script.js
+++ script.js
@@ -1,16 +1,39 @@
 var today = moment();
 var hour = moment().hour();
-var text = $(".description");
-var containerEl = $(".container");
-var saveBtn = $('.saveBtn');
-var timeList = document.querySelectorAll('.hour');
+var textEl = document.querySelectorAll(".description");
+var saveBtn = document.querySelectorAll(".saveBtn");
+
+// shows the current date at the top of the page in the proper format
 
 $("#currentDay").text(today.format("LLL"));
-console.log(timeList.innerText);
 
-for (let i = 0; i < timeList.length; i++) {
-    if (timeList.innerText != hour) {
-        console.log('random text');
-    }
-    
-}
+/* uses a forEach loop to create a scenario where if the id value for each text element is greater than or less than the current hour a class of future of past will be
+assigned otherwise they will be considered the present*/
+
+textEl.forEach((time) => {
+  if (time.id > hour) {
+    time.classList.add("future");
+  } else if (time.id < hour) {
+    time.classList.add("past");
+  } else {
+    time.classList.add("present");
+  }
+});
+
+/* uses another forEach loop where for each save button there was an event listener that could store all the inputed values in all the text fields of the document.
+The stored values are stringified and parsed back out to their places in the page whenever reloaded*/
+
+saveBtn.forEach((save, i) => {
+  save.addEventListener("click", (event) => {
+    event.preventDefault();
+    var store = Array.from(textEl, (text) => text.value);
+    var eachTxt;
+    textEl.forEach((inputArea) => {
+      if (inputArea.id === event.target.name) {
+        eachTxt = inputArea;
+      }
+    });
+    localStorage.setItem("Save", JSON.stringify(store));
+  });
+  textEl[i].value = JSON.parse(localStorage.getItem("Save"))[i];
+});
